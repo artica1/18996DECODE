31967ff6dddd23e12fcbc0653754c223d85e1a4d
Arjun <arjun.sieracki@icloud.com>
2025-09-09 22:30:02 -0400

    Added HardwareMapNames and XML configuration file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapNames.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class HardwareMapNames {
+    // EXAMPLE
+    // public static String INTAKE_MOTOR = "intakeMotor";
+}
