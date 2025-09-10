13a855540fbfe1c4132c9b6ab70777b3c6cff941
Arjun <arjun.sieracki@icloud.com>
2025-09-09 22:35:09 -0400

    Add Drive framework

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Drive.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.seattlesolvers.solverslib.drivebase.MecanumDrive;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+
+public class Drive extends MecanumDrive {
+    private AutoDrive autoDrive;
+    private ManualDrive manualDrive;
+
+    public Drive(Motor frontLeft, Motor frontRight, Motor backLeft, Motor backRight) {
+        super(frontLeft, frontRight, backLeft, backRight);
+    }
+}
