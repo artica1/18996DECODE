9996972f37e8e19e0cf41f51b5e722b661390c11
christopheraustin <christopheraustin3088@gmail.com>
2025-11-15 18:56:17 -0500

    shooter interpolation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automations/ShooterCalculations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automations/ShooterCalculations.java
@@ -1,5 +1,24 @@
 package org.firstinspires.ftc.teamcode.automations;
 
+import com.seattlesolvers.solverslib.util.InterpLUT;
+
 public class ShooterCalculations {
-// static class{
+    private static final InterpLUT angleILUT = new InterpLUT();
+    private static final InterpLUT speedILUT = new InterpLUT();
+
+    public static double getSpeed(double dist) {
+        return speedILUT.get(dist);
+    }
+
+    public static double getAngle(double angle) {
+        return angleILUT.get(angle);
+    }
+
+    public ShooterCalculations() {
+        // add data
+
+
+        speedILUT.createLUT();
+        angleILUT.createLUT();
+    }
 }
